a64560c8cc16cf2e9d39284bef27e0b3a55f9292
joseunghyo <seunghyo7742@naver.com>
2023-03-06 12:49:26 +0900

    [Add] add LoginPage #1

---
+++ kiwe_mvp-app/src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+const LoginPage=()=>{
+  return(
+    <>
+      <div class="text-top">
+        <h2 class="logo">KiWES</h2>
+        <h2 class="start">시작하기</h2>
+      </div>
+      <img id="logo-img" src="" />
+      <button>카카오로그인하기들어가야함</button>
+    </>
+  )
+}
---
+++ kiwe_mvp-app/src/pages/ProfileSettingPage.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const ProfileSettingPage=()=>{
+  const [profileImg, setProfileImg]= useState('');
+  const[name, setName]=useState('');
+  const[checkName,setCheckName]=useState(false);
+  
+  return(
+
+  )
+}
