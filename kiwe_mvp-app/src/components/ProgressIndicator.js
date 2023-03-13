2f445903e52c3cef93ddca3764a38eb25f39364d
joseunghyo <seunghyo7742@naver.com>
2023-03-13 09:30:22 +0900

    [Edit] edit css #2

---
+++ kiwe_mvp-app/src/components/ProgressIndicator.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+const ProgressIndicator=(props)=>{
+  const[progress, setProgress]=useState(props.progress||0);
+  return(<header style={{position:"fixed", zIndex:"999", top:0, left:0, right:0 , backgroundColor:"white", paddingTop:"15%"}}>
+    <div className="progress-bar"
+      style={{
+        height:"4px",
+        width:"40.4%",
+        margin:"0 auto",
+        backgroundColor:"#ddd",
+        position:"relative",
+        
+      }}>
+      <div
+        className="progress-bar-fill"
+        style={{width: `${progress}%`,
+        height:"100%",
+        backgroundColor:"#0f161e",
+        position:"absolute",
+        left:"0",
+        top:"0"
+      }}
+      />
+    </div>
+  </header>);
+}
+export default ProgressIndicator;
+
--- kiwe_mvp-app/src/pages/ProfileSettingPage.js
+++ kiwe_mvp-app/src/pages/ProfileSettingPage.js
@@ -1,9 +1,21 @@
 import React, { useState } from 'react';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import '../styles/profileSetting.css';
+import Footer from '../Footer';
+import ProgressIndicator from "../components/ProgressIndicator";
+import Badge from '@mui/material/Badge';
+import Avatar from '@mui/material/Avatar';
+import { styled } from '@mui/material/styles';
+
+const SmallAvatar = styled(Avatar)(({ theme }) => ({
+  width: 26,
+  height: 26,
+  opacity:0.7,
+}));
 
 const ProfileSettingPage=()=>{
-  const [profileImg, setProfileImg]= useState('');//default이미지경로 추후
+  const [profileImg, setProfileImg]= useState('/assets/profile_default.png');//default이미지경로 추후
   const[name, setName]=useState('');
   const [gender, setGender]= useState("");
   const[checkName,setCheckName]=useState(false);
@@ -17,29 +29,40 @@ const ProfileSettingPage=()=>{
 
   return(
     <div>
-      <div className="text-top">
-          <h2 className="logo">KiWES</h2>
-          <h2 className="start">프로필 설정</h2>
+    <ProgressIndicator progress={35}/>
+      <div className="logobox_setting">
+          <img src="/assets/kiwes_logo.png" alt="키위새"/>
+          <span className="title_setting">KiWES</span>
+          <span className="page-title">프로필 설정</span>
       </div>
-      <form onSubmit={handleSubmit}>
-        <div className="profile-setting">
-          <div className="input label">프로필 이미지</div>
+      <form className="profile-form-box" onSubmit={handleSubmit}>
+        <div className="img-setting">
+          <div className="input-label">프로필 이미지</div>
+          <Badge
+            overlap="circular"
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+            badgeContent={
+              <SmallAvatar alt="edit-button" src="/assets/edit-button.png" />
+            }
+          >
+            <Avatar alt="profileImg" src={profileImg} sx={{ width: 80, height:80 }}/>
+          </Badge>
           <input id="setProfileImg" type="file" onChange={(event)=>setProfileImg(event.target.files[0].name)}/>
         </div>
 
         <div className="name-setting">
-          <div className="input label">닉네임</div>
+          <div className="input-label">닉네임</div>
           <input id="setName" type="text" value={name} onChange={(event)=>setName(event.target.value)} required/>
         </div>
 
         <div className="gender-setting">
-          <div className="input label">성별 *</div>
+          <div className="input-label">성별 *</div>
           <label><input type="radio" value="남" checked={gender==="male"} onChange={(event)=>setGender(event.target.value)}></input></label>
           <label><input type="radio" value="여" checked={gender==="female"} onChange={(event)=>setGender(event.target.gender)}></input></label>
         </div>
         
         <div className="birth-setting">
-          <div className="input label">생년월일 *</div>
+          <div className="input-label">생년월일 *</div>
           <DatePicker selected={birthdate} onChange={(date)=>setBirthdate(date)}
           dateFormat="yyyy/MM/dd"
           showYearDropdown
@@ -50,13 +73,13 @@ const ProfileSettingPage=()=>{
         </div>
 
         <div className="email-auth">
-          <div className="input email">소속 대학 메일 인증 (선택)</div>
+          <div className="input-label">소속 대학 메일 인증 (선택)</div>
           <input id="setEmail" type="email" value={email} onChange={(event)=>setEmail(event.target.value)}/>
         </div>
         <button className="btn-next" type="submit">설정 완료</button>
 
         <div className="bio-setting">
-          <div className="input bio">자기소개 (선택)</div>
+          <div className="input-label">자기소개 (선택)</div>
           <textarea id="setBio" type="text" value={bio} rows="5" cols="33" onChange={(event)=>setBio(event.target.value)} placeholder="자기소개를 입력해주세요.(150자 제한)"></textarea>
         </div>
       </form>
