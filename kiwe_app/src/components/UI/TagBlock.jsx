e7a7046caf5ec00681339185b62fe238c0645ad1
joseunghyo <seunghyo7742@naver.com>
2023-03-19 03:05:59 +0900

    [Feat] improve reusability #4

--- kiwe_app/src/components/UI/TagBlock.jsx
+++ kiwe_app/src/components/UI/TagBlock.jsx
@@ -2,18 +2,21 @@ import React from 'react'
 
 export default function TagBlock(props) {
   const tagBlockStyles = {
-    backgroundColor: props.color || 'rgba(0, 0, 0, 0.25)',
+    backgroundColor: props.color || 'white',
     width: "auto",
+    minWidth: "3.5rem",
     display: "inline-flex",
     alignItems: "center",
     justifyContent: "center",
     padding: "0.2rem",
     borderRadius: "30px",
-    boxShadow: "0 3px 3px 0",
+    boxShadow: props.boxShadow== 1 ? "0 3px 3px 0" : "none", 
+    border: props.borderColor==0? "none": 'solid 1px #3dbe14', 
     fontSize: "0.75rem",
-    fontWeight:"600",
-    color:"#040404",
+    fontWeight: "600",
+    color: "#040404",
   };
+  
   return (
     <div className="tagBlockCover" style={tagBlockStyles}>
       {props.children}
