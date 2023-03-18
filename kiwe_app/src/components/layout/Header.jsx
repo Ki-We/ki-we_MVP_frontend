8629c3b3062a18ef2d4928e9ba58049cfa2d207f
MinsuKim <minsu020302@gmail.com>
2023-03-19 02:52:08 +0900

    Feat : 헤더 완료

--- kiwe_app/src/components/layout/Header.jsx
+++ kiwe_app/src/components/layout/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import '../../styles/langPage.css';
+import '../../global.css';
 
 export default function Header() {
   return (
-    <header style={{ display: 'flex', justifyContent: 'space-around' }}>
-      <div className="logobox">
-        <img src="/assets/kiwes_logo.png" alt="키위새" />
-        <span className="title">KiWES</span>
+    <header className="flex flex-row w-screen px-6 py-5 z-50">
+      <div className="w-full h-10 flex algin">
+        <img className="trasform scale-x-[-1]" src="/assets/kiwes_logo.png" alt="키위새" />
+        <span className=" font-extrabold text-myGreen  text-3xl">KiWES</span>
       </div>
-      <div style={{ width: '' }}>
+      <div className="w-9 my-auto">
         <img src="/assets/ring.png" alt="알림" />
       </div>
     </header>
