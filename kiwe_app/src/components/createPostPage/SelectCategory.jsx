99d0bcdfd18d3084f75d0fcc9b3e2b81d0020298
MinsuKim <minsu020302@gmail.com>
2023-03-19 03:45:31 +0900

    Feat : 카테고리 selector UI완성 #4

---
+++ kiwe_app/src/components/createPostPage/SelectCategory.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function SelectCategory() {
+  return (
+    <div className="px-4 relative my-1">
+      <h6 className=" font-bold">카테고리</h6>
+      <div className=" flex justify-around my-1 space-x-3"></div>
+    </div>
+  );
+}
